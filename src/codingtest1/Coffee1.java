4bdc72178f33b23f804587ab12b675ccb64a9169
Mirim <s2113@e-mirim.hs.kr>
2022-12-26 11:42:50 +0900

    Comparable 예제 Coffee1 get set 메소드

---
+++ src/codingtest1/Coffee1.java
@@ -0,0 +1,19 @@
+package codingtest1;
+
+public class Coffee1 {
+    private final String name;
+    private final int price;
+
+    public Coffee1(String name, int price){
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getPrice(){
+        return price;
+    }
+}
