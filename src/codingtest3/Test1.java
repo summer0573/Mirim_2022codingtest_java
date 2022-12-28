4638c3ef277fd77303364cb07440ca0bfcf57aee
Mirim <s2113@e-mirim.hs.kr>
2022-12-28 10:09:48 +0900

    구명보트 코딩테스트 정답, 기지국 설치 코딩테스트

--- src/codingtest3/Test1.java
+++ src/codingtest3/Test1.java
@@ -1,5 +1,7 @@
 package codingtest3;
 
+import java.util.Arrays;
+//구명보트
 class Solution{
     public int solution(int[] people, int limit){
         //people : 몸무게 목록
@@ -14,19 +16,30 @@ class Solution{
         int last = people.length - 1;
         
         //정렬 (sort() => 오름차순 정렬 => 작은것부터
+        //50, 50, 70, 80
+        //50, 50, 70, 30
+        Arrays.sort(people);
         
         //가벼운 몸무게에서 가장 큰 몸무게의 합이 limit범위에 들어가는지 체크하고
         //limit보다 작으면 first를 증가하면서
         //크면 last를 감소하면서 비교
+        while (first <= last){
+            answer++;
+            if(people[first]+people[last]<=limit){
+                answer++;
+                first++;
+                last--;
+            } else {
+                last--;
+            }
+        }
 
-        
-        
         return answer;
     }
 }
 
 public class Test1 {
     public static void main(String[] args){
-        
+
     }
 }
---
+++ src/codingtest3/Test2.java
@@ -0,0 +1,13 @@
+package codingtest3;
+
+
+//기지국 설치
+class solution{
+
+}
+
+public class Test2 {
+    public static void main(String[] args){
+
+    }
+}
