7da64945bbb48169ca26f86bb488fe452f0367e5
Mirim <s2113@e-mirim.hs.kr>
2022-12-28 11:32:28 +0900

    기지국 설치 코딩테스트 정답

--- src/codingtest3/Test2.java
+++ src/codingtest3/Test2.java
@@ -5,25 +5,28 @@ package codingtest3;
 class Solution2 {
     public int solution2(int n, int[] stations, int w) {
         int answer = 0;
-
         //n : 아파트 동 갯수
         //stations : 기지국이 설치된 위치
         //w : 전파 범위
 
         //배열로 접근
         //배열에 접근할 수 있는 인덱스 번호
-
+        int index = 0;
         //현재 기지국이 설치된 값을 체크할 변수
-
+        int position = 1;
         //현재 위치에서 아파트 동을 반복하면서
-
-        //1)기존에 기지국이 설치가 되어 있는 경우
-
-        //2)새롭게 설치하는 경우
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
+        while(position <= n) {
+            //1)기존에 기지국이 설치가 되어 있는 경우 > 전파 범위 체크
+            if(index<stations.length && stations[index]-w <= position){
+                position = stations[index] + w + 1;
+                index++;
+            }
+            //2)새롭게 설치하는 경우
+            else {
+                answer++;
+                position += 2*w+1;
+            }
+        }
         return answer;
     }
 }
